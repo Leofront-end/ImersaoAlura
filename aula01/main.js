ee99523bd87728dfe587c2d9730287a56f8655e7
Leofront-end <ls171258@gmail.com>
2025-03-31 16:43:19 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,8 @@
+// console.log("oi")
+// alert("Olá mundo")
+// alert(10 + 2)
+
+valorWons = prompt("Digite um valor em wons: ")
+// alert(valorWons)
+umWon = 0.0040
+alert("R$ "+ umWon * valorWons)
